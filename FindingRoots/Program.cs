8c6f5c4df2c80b1a44860ad0fd9d91b320c6de6f
Elena Bakova <lenabakova72@gmail.com>
2022-09-08 00:19:13 +0300

    Added roots separation

--- FindingRoots/Program.cs
+++ FindingRoots/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FindingRoots;
+
+int A = 0;
+int B = 0;
+int N = 0;
+
+while (true)
+{
+    Console.WriteLine("Please enter A - left bound and B - right bound");
+    var read = Console.ReadLine()?.Split(' ', StringSplitOptions.RemoveEmptyEntries) ?? Array.Empty<string>();
+    if (read.Length < 2 || !int.TryParse(read[0], out A) || !int.TryParse(read[1], out B))
+    {
+        Console.WriteLine("Invalid input\nPlease try again\n");
+        continue;
+    }
+
+    Console.WriteLine("Please enter N");
+    read = Console.ReadLine()?.Split(' ', StringSplitOptions.RemoveEmptyEntries) ?? Array.Empty<string>();
+    if (read.Length < 1 || !int.TryParse(read[0], out N) || N < 0)
+    {
+        Console.WriteLine("Invalid input\nPlease try again\n");
+        continue;
+    }
+    //Console.WriteLine($"{A} {B} {N}");
+
+    var separation = new RootsSeparation(A, B, N, x => x - 10);// * Math.Sin(x));
+}
--- FindingRoots/RootsSeparation.cs
+++ FindingRoots/RootsSeparation.cs
@@ -1,27 +1,54 @@
 ﻿namespace FindingRoots
 {
+    /// <summary>
+    /// Class for separating roots
+    /// </summary>
     public class RootsSeparation
     {
-        //public (int left, int right) Boundaries { get; private init; }
-        private int A;
-        private int B;
-        private readonly int N;
-        private int counter = 0;
-        private Func<int, int> func;
+        public List<(int left, int right)> Result { get; set; } = new List<(int left, int right)> ();
+
+       // private readonly int N;
+       // private int counter = 0;
+        private readonly int A;
+        private readonly int B;
+        private readonly Func<int, int> func;
         private readonly int step;
 
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        /// <param name="A">Left bound of the segment</param>
+        /// <param name="B">Right bound of the segment</param>
+        /// <param name="N">Constant value for counting step</param>
+        /// <param name="func">Function</param>
         public RootsSeparation(int A, int B, int N, Func<int, int> func)
         {
+            //this.N = N;
             this.A = A;
             this.B = B;
-            this.N = N;
             this.func = func;
             step = (B - A) / N;
+            Separation();
         }
 
         public void Separation()
         {
-            
+           // List<(int left, int right)> boundaries = new();
+            int x1 = A;
+            int x2 = x1 + step;
+            int y1 = func(x1);
+            while (x2 <= B)
+            {
+                int y2 = func(x2);
+                if (y1 * y2 <= 0)
+                {
+                   // counter++;
+                    Result.Add((x1, x2));
+                }
+                x1 = x2;
+                x2 += step;
+                y1 = y2;
+            }
         }
     }
 }
