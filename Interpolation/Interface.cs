01132937f15d348fbb620ef0fcc61f608e2e8678
Elena Bakova <lenabakova72@gmail.com>
2022-09-23 23:46:17 +0300

    Added interface functions

--- Interpolation/Interface.cs
+++ Interpolation/Interface.cs
@@ -2,15 +2,38 @@
 
 public static class Interface
 {
+    // m + 1
     private static int numberOfValues;
     private static (int a, int b) segment;
-    private static float interpolationPoint;
+    private static double interpolationPoint;
+    private static int degreeOfPolynomial;
+    private static readonly List<(double x, double result)> interpolationTable = new();
+    private static readonly Func<double, double> func = x =>
+    {
+        return Math.Sin(x) - (x * x / 2);
+    };
 
     public static void Run()
     {
         GetNumberOfValues();
         GetBoundaries();
+        BuildTable();
+
+        Console.WriteLine("Initial table of points");
+        interpolationTable.ForEach(item => Console.WriteLine($"{item.x} -- {item.result}"));
+
         GetPoint();
+        GetDegree();
+    }
+
+    private static void BuildTable()
+    {
+        interpolationTable.Clear();
+        for (int i = 0; i < numberOfValues; i++)
+        {
+            double point = segment.a + (i * ((segment.b * 1.0) - segment.a) / (numberOfValues - 1.0));
+            interpolationTable.Add((point, func(point)));
+        }
     }
 
     private static void GetNumberOfValues()
@@ -34,15 +57,32 @@ public static class Interface
             GetBoundaries();
             return;
         }
+        if (segment.a > segment.b)
+        {
+            Console.WriteLine("Left bound should be less than right. Please, try again");
+            GetBoundaries();
+            return;
+        }
     }
 
     private static void GetPoint()
     {
         Console.WriteLine("Please, enter interpolation point");
-        if (float.TryParse(Console.ReadLine(), out interpolationPoint) == false)
+        if (double.TryParse(Console.ReadLine(), out interpolationPoint) == false)
         {
             Console.WriteLine("Invalid input. Please, try again");
             GetPoint();
+            return;
+        }
+    }
+
+    private static void GetDegree()
+    {
+        Console.WriteLine("Please, enter degree of polynomial. It should be less than number of values");
+        if (int.TryParse(Console.ReadLine(), out degreeOfPolynomial) == false || degreeOfPolynomial > numberOfValues)
+        {
+            Console.WriteLine("Invalid input. Please, try again");
+            GetDegree();
             return;
         }
     }
--- Interpolation/Program.cs
+++ Interpolation/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Interpolation;
 
-Console.WriteLine("Problem of algebraic interpolation\nVariant 1\n");
+Console.WriteLine("Problem of algebraic interpolation\nVariant 1");
+Console.WriteLine("f(x) = sin(x) - x^2 / 2; m = 15; n = 7; a = 0; b = 1; x = 0.65\n");
 Interface.Run();
