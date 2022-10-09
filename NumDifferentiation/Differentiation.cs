9840497a6a1813cb3630cbfd742f6059952292e0
Elena Bakova <lenabakova72@gmail.com>
2022-10-09 20:30:45 +0300

    WIP

--- NumDifferentiation/Differentiation.cs
+++ NumDifferentiation/Differentiation.cs
@@ -1,58 +1,47 @@
-﻿using System;
-
-namespace NumDifferentiation;
+﻿namespace NumDifferentiation;
 
 public class Differentiation
 {
     /// <summary>
     /// Initial function
     /// </summary>
-    private static readonly Func<double, double> function = x => Math.Exp(3 * x);
-
-    /// <summary>
-    /// First derivative of function
-    /// </summary>
-    private static readonly Func<double, double> firstDerivative = x => 3 * Math.Exp(3 * x);
-
-    /// <summary>
-    /// Second derivative
-    /// </summary>
-    private static readonly Func<double, double> secondDerivative = x => 9 * Math.Exp(3 * x);
+    private static Func<double, double> function = x => x;
 
     private static List<(double x, double y)> tableOfPoints = new();
 
-    public Differentiation(List<(double x, double y)> table)
+    public Differentiation(List<(double x, double y)> table, Func<double, double> func)
     {
         tableOfPoints = table;
+        function = func;
     }
 
-    public static List<(double x, double y)> FirstDerivative()
+    public List<double> FirstDerivative()
     {
-        List<(double x, double y)> result = new();
+        List<double> result = new();
 
         for (int i = 0; i < tableOfPoints.Count; i++)
         {
-            result.Add((tableOfPoints[i].x, Derivative(i)));
+            result.Add(Derivative(i));
         }
 
         return result;
     }
 
-    public static List<(double x, double y)> SecondDerivative()
+    public List<double> SecondDerivative()
     {
-        List<(double x, double y)> result = new();
+        List<double> result = new();
 
         for (int i = 0; i < tableOfPoints.Count; i++)
         {
             if (i == 0 || i == tableOfPoints.Count - 1)
             {
-                result.Add((tableOfPoints[i].x, -1));
+                result.Add(-1);
             }
             else
             {
                 var derivative = (tableOfPoints[i + 1].y - 2 * tableOfPoints[i].y + tableOfPoints[i - 1].y)
                                  / Math.Pow(tableOfPoints[i + 1].x - tableOfPoints[i].x, 2); ;
-                result.Add((tableOfPoints[i].x, derivative));
+                result.Add(derivative);
             }
         }
         return result;
--- NumDifferentiation/Interface.cs
+++ NumDifferentiation/Interface.cs
@@ -17,9 +17,25 @@ public static class Interface
     /// </summary>
     private static double step;
 
+    /// <summary>
+    /// Initial function
+    /// </summary>
+    private static readonly Func<double, double> function = x => Math.Exp(3 * x);
+
+    /// <summary>
+    /// First derivative of function
+    /// </summary>
+    private static readonly Func<double, double> firstDerivative = x => 3 * Math.Exp(3 * x);
+
+    /// <summary>
+    /// Second derivative
+    /// </summary>
+    private static readonly Func<double, double> secondDerivative = x => 9 * Math.Exp(3 * x);
+
+    private static List<double> firstDerivativeValues = new();
+    private static List<double> secondDerivativeValues = new();
+
     private static readonly List<(double x, double y)> tableOfPoints = new();
-    private static readonly int k = 2;
-    private static readonly Func<double, double> func = x => Math.Exp(1.5 * k * x);
 
     public static void Run()
     {
@@ -29,7 +45,14 @@ public static class Interface
         BuildTable();
 
         Console.WriteLine("Initial table of points");
+        tableOfPoints.Sort((first, second) => first.x.CompareTo(second.x));
         tableOfPoints.ForEach(item => Console.WriteLine($"{item.x} -- {item.y}"));
+
+        var instance = new Differentiation(tableOfPoints, function);
+        firstDerivativeValues = instance.FirstDerivative();
+        secondDerivativeValues = instance.SecondDerivative();
+
+        PrintInfoTable();
 
         Console.WriteLine("\nWould you like to change data?\nY - start over\nN - exit");
         while (true)
@@ -44,6 +67,27 @@ public static class Interface
             {
                 return;
             }
+        }
+    }
+
+    private static void PrintInfoTable()
+    {
+        Console.WriteLine("  x  /  f(x)  / f'(x)nd /  |f'(x)-f'(x)nd|  /  f' error  / f''(x)nd /  |f''(x)-f''(x)nd|  /  f'' error");
+        for (int i = 0; i < tableOfPoints.Count; i++)
+        {
+            var secondDerivativeOutput = "";
+            if (i == 0 || i == tableOfPoints.Count - 1)
+            {
+                secondDerivativeOutput = $"  --  /  --  /  --";
+            }
+            else
+            {
+                var error = Math.Abs(secondDerivative(tableOfPoints[i].x) - secondDerivativeValues[i]);
+                secondDerivativeOutput = $" {secondDerivativeValues[i]}  / {error} / {error / secondDerivative(tableOfPoints[i].x)}";
+            }
+
+            var temp = Math.Abs(firstDerivative(tableOfPoints[i].x) - firstDerivativeValues[i]);
+            Console.WriteLine($" {tableOfPoints[i].x} / {tableOfPoints[i].y} / {firstDerivativeValues[i]} / {temp} / {temp / firstDerivative(tableOfPoints[i].x)} / {secondDerivativeOutput}");
         }
     }
 
@@ -80,7 +124,7 @@ public static class Interface
         for (int i = 0; i < numberOfValues; i++)
         {
             double point = startPoint + (i * step);
-            tableOfPoints.Add((point, func(point)));
+            tableOfPoints.Add((point, function(point)));
         }
     }
 }
