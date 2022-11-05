f0092b7f082e1eaa723e9d1dad870a8d874a1f4e
Elena Bakova <lenabakova72@gmail.com>
2022-11-06 00:47:17 +0300

    Renamed dir

--- Interpolation/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using Interpolation;
-
-Console.WriteLine("Problem of algebraic interpolation\nVariant 1");
-Console.WriteLine("f(x) = sin(x) - x^2 / 2; m = 15; n = 7; a = 0; b = 1; x = 0.65\n");
-
-Interface.Run();
--- NumDifferentiation/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using NumDifferentiation;
-
-Console.WriteLine("Problem of numerical differentiation\nVariant 1");
-Console.WriteLine("f(x) = exp(1,5 * k * x); k = 2\n");
-
-Interface.Run();
