ce7222e1f5f01792771c645a20d8d51622a5a6ef
Elena Bakova <lenabakova72@gmail.com>
2022-11-07 12:15:54 +0300

    wip

---
+++ 4.1/InitialData.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+
+namespace _4._1;
+public class InitialData
+{
+    private static readonly Func<double, double> function = x => Math.Cos(x) * Math.Sqrt(x);
+    private static readonly string functionString = "cos(x)*sqrt(x)";
+    private static readonly (double a, double b) limits = new() { a = 0, b = 1};
+
+    public static string FuncString => functionString;
+    public double Antiderivative { get; init; }
+    public double Simpsons { get; init; }
+    public double InterpolationFormula { get; init; }
+
+    public InitialData()
+    {
+        var temp = GetAntiderivative();
+        Antiderivative = temp.Item1;
+        Simpsons = (limits.b - limits.a) / 6 * (function(limits.a) + function(limits.b) + (4 * function((limits.b + limits.a) / 2)));
+        
+    }
+
+    private void GetInterpolationFormula()
+    {
+
+    }
+
+    private static (double, double) GetAntiderivative()
+    {
+        string scriptPath = Environment.CurrentDirectory + "\\..\\..\\..\\script";
+        ProcessStartInfo startInfo = new("python");
+
+        string directory = scriptPath;
+        string script = "main.py";
+
+        startInfo.WorkingDirectory = directory;
+        startInfo.Arguments = $"{script} {functionString} {limits.a} {limits.b}";
+        startInfo.UseShellExecute = false;
+        startInfo.CreateNoWindow = true;
+        startInfo.RedirectStandardError = true;
+        startInfo.RedirectStandardOutput = true;
+
+        (double, double) result = new();
+        using (Process process = Process.Start(startInfo))
+        using (StreamReader reader = process.StandardOutput)
+        {
+            string line = reader.ReadLine();
+            result.Item1 = double.Parse(line);
+            line = reader.ReadLine();
+            result.Item2 = double.Parse(line);
+            /*string foo = reader.ReadToEnd();
+            result += foo;*/
+        }
+
+        return result;
+    }
+}
---
+++ 4.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using _4._1;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Лабораторная работа 4.1\nВариант 1\nПриближённое вычисление интеграла по составным квадратурным формулам\n\n");
+
+var data = new InitialData();
+
+Console.WriteLine($"Интегрируемая функция: {InitialData.FuncString}\n");
+Console.WriteLine($"'Точное' значение интеграла: {data.Antiderivative}\n\n");
+
+Console.WriteLine($"Значение интеграла по формуле Симпсона с тремя узлами: {data.Simpsons}\n");
+Console.WriteLine($"Абсолютная фактическая погрешность: {Math.Abs(data.Simpsons - data.Antiderivative)}\n");
