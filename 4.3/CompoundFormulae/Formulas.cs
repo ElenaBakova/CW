152c1ab0b635352814979a408b97eb3ddcf2f033
Elena Bakova <lenabakova72@gmail.com>
2022-11-11 16:48:15 +0300

    Added 4.3

---
+++ 4.3/CompoundFormulae/Formulas.cs
@@ -0,0 +1,37 @@
+﻿namespace CompoundFormulae;
+
+public class Formulas
+{
+    private static readonly Func<double, double> function = x => Math.Log(1 + x);
+    private static readonly Func<double, double> antiderivative = x => (1 + x) * Math.Log(1 + x) - x;
+
+    /*private static readonly Func<double, double> function = x => x;
+    private static readonly Func<double, double> antiderivative = x => x * x / 2;*/
+
+    /*private static readonly Func<double, double> function = x => x * x;
+    private static readonly Func<double, double> antiderivative = x => x * x * x / 3;*/
+
+    /*private static readonly Func<double, double> function = x => x * x * x;
+    private static readonly Func<double, double> antiderivative = x => x * x * x * x / 4;*/
+
+    public double PreciseValue = antiderivative(1) - antiderivative(0);
+    public double LeftRectangle { get; init; }
+    public double RightRectangle { get; init; }
+    public double MiddleRectangle { get; init; }
+    public double Trapezoidal { get; init; }
+    public double Simpsons { get; init; }
+    public double ThreeEighths { get; init; }
+
+    public Formulas((double a, double b) limits)
+    {
+        LeftRectangle = (limits.b - limits.a) * function(limits.a);
+        RightRectangle = (limits.b - limits.a) * function(limits.b);
+        MiddleRectangle = (limits.b - limits.a) * function((limits.a + limits.b) / 2);
+        Trapezoidal = (limits.b - limits.a) / 2 * (function(limits.a) + function(limits.b));
+
+        double h = (limits.b - limits.a) / 3.0;
+        Simpsons = h / 2 * (function(limits.a) + function(limits.b) + (4 * function((limits.a + limits.b) / 2)));
+        ThreeEighths = (limits.b - limits.a) * ((function(limits.a) / 8.0) + (function(limits.b) / 8.0)
+            + (3.0 / 8 * function(limits.a + h)) + (3.0 / 8 * function(limits.a + (2 * h))));
+    }
+}
---
+++ 4.3/CompoundFormulae/Interface.cs
@@ -0,0 +1,66 @@
+﻿namespace CompoundFormulae;
+
+public class Interface
+{
+    private static (double a, double b) limits;
+
+    public static void Run()
+    {
+        GetLimits();
+        var integrals = new Formulas(limits);
+        PrintValues(integrals);
+
+        Console.WriteLine("\nВвести новые данные?\nY - да\nN - нет");
+        while (true)
+        {
+            string read = Console.ReadLine() ?? "";
+            if (read == "Y" || read == "y")
+            {
+                Run();
+                return;
+            }
+            else if (read == "N" || read == "n")
+            {
+                return;
+            }
+        }
+    }
+
+    private static void PrintValues(Formulas integrals)
+    {
+        double j = integrals.PreciseValue;
+
+        Console.WriteLine(
+                $"\nТочное значение: {integrals.PreciseValue}\n" +
+                $"Формула левых прямоугольников: {integrals.LeftRectangle}\n" +
+                $"Формула правых прямоугольников: {integrals.RightRectangle}\n" +
+                $"Формула средних прямоугольников: {integrals.MiddleRectangle}\n" +
+                $"Формула трапеций: {integrals.Trapezoidal}\n" +
+                $"Формула Симпсона: {integrals.Simpsons}\n" +
+                $"Формула трех восьмых: {integrals.ThreeEighths}\n");
+
+        Console.WriteLine("Погрешности");
+        Console.WriteLine(
+                $"Формула левых прямоугольников: {Math.Abs(j - integrals.LeftRectangle)}\n" +
+                $"Формула правых прямоугольников: {Math.Abs(j - integrals.RightRectangle)}\n" +
+                $"Формула средних прямоугольников: {Math.Abs(j - integrals.MiddleRectangle)}\n" +
+                $"Формула трапеций: {Math.Abs(j - integrals.Trapezoidal)}\n" +
+                $"Формула Симпсона: {Math.Abs(j - integrals.Simpsons)}\n" +
+                $"Формула трех восьмых: {Math.Abs(j - integrals.ThreeEighths)}\n");
+    }
+
+    private static void GetLimits()
+    {
+        Console.Write("Введите левый конец отрезка интегрирования: ");
+        while (!double.TryParse(Console.ReadLine(), out limits.a))
+        {
+            Console.Write("Некорректное значение: введите вещественное число: ");
+        }
+
+        Console.Write("Введите правый конец отрезка интегрирования: ");
+        while (!double.TryParse(Console.ReadLine(), out limits.b) || limits.b <= limits.a)
+        {
+            Console.Write("Некорректное значение: введите вещественное число, большее левого конца отрезка интегрирования: ");
+        }
+    }
+}
