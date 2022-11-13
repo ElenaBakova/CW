e540207f3ef2309af92ee3acbdade7e3d4b7e97d
Elena Bakova <lenabakova72@gmail.com>
2022-11-13 16:22:54 +0300

    smth

---
+++ 4.4/Integration/Program.cs
@@ -0,0 +1,7 @@
+﻿using CompoundFormulae;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Лабораторная работа 4.3\nПриближённое вычисление интеграла по составным квадратурным формулам\n\n");
+
+Interface.Run();
